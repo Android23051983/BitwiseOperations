b7e55962b386fe8611b9458a27c6d9e7b406ee5d
Andrei <ikar2008@hotmail.com>
2023-01-20 15:46:44 +0300

    Add project files.

---
+++ BitwiseOperations/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace BitwiseOperations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введиьте число";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(178, 18);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 68);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(286, 22);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Двоичная система счисления";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 109);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(366, 22);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Шеснадцатеричная система счисления";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(23, 151);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(326, 22);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Восьмиричная система счисления";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 203);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Выберите систему счисления";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Начальное число";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(184, 242);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "16";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Сдвиговое число";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(184, 283);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "2";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.Location = new System.Drawing.Point(23, 329);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(56, 22);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "127";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton5.Location = new System.Drawing.Point(184, 329);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(56, 22);
+            this.radioButton5.TabIndex = 12;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "255";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton6.Location = new System.Drawing.Point(23, 369);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(56, 22);
+            this.radioButton6.TabIndex = 13;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "528";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton7.Location = new System.Drawing.Point(184, 369);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(56, 22);
+            this.radioButton7.TabIndex = 14;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "614";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 394);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(278, 18);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Результат вычисления числа:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(23, 412);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(400, 26);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.Text = "Выберите число которое хотите получить";
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton8.Location = new System.Drawing.Point(293, 329);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(56, 22);
+            this.radioButton8.TabIndex = 17;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "AND";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton9.Location = new System.Drawing.Point(263, 369);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(46, 22);
+            this.radioButton9.TabIndex = 18;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "OR";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.radioButton9_CheckedChanged);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton10.Location = new System.Drawing.Point(324, 369);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(56, 22);
+            this.radioButton10.TabIndex = 19;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "XOR";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(484, 465);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Система счисления + поразрядные операции ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.Label label5;
+        protected internal System.Windows.Forms.TextBox textBox1;
+        protected internal System.Windows.Forms.TextBox textBox3;
+        protected internal System.Windows.Forms.TextBox textBox4;
+        protected internal System.Windows.Forms.TextBox textBox5;
+        protected internal System.Windows.Forms.RadioButton radioButton8;
+        protected internal System.Windows.Forms.RadioButton radioButton9;
+        protected internal System.Windows.Forms.RadioButton radioButton10;
+    }
+}
+
---
+++ BitwiseOperations/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BitwiseOperations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+            textBox2.Clear();
+            textBox2.Text = Convert.ToString(i, 2);
+            
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+            textBox2.Clear();
+            textBox2.Text += Convert.ToString(i, 16);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+            textBox2.Clear();
+            textBox2.Text = Convert.ToString(i, 8);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int i;
+            if(String.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Text = "0";
+                i = Convert.ToInt32(textBox1.Text);
+            }
+            else
+            {
+                i = Convert.ToInt32(textBox1.Text);
+            }
+            if (radioButton1.Checked == true)
+            {
+                textBox2.Clear();
+                textBox2.Text = Convert.ToString(i, 2);
+
+            }
+            else if (radioButton2.Checked == true) 
+            {
+                textBox2.Clear();
+                textBox2.Text += Convert.ToString(i, 16);
+
+            }
+            else if(radioButton3.Checked == true)
+            {
+                textBox2.Clear();
+                textBox2.Text = Convert.ToString(i, 8);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox3.Text) | String.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox3.Text = "16";
+                textBox4.Text = "2";
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 & num2;
+                textBox5.Text = result.ToString();
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 & num2;
+                textBox5.Text = result.ToString();
+            }
+
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox3.Text) | String.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox3.Text = "16";
+                textBox4.Text = "2";
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 | num2;
+                textBox5.Text = result.ToString();
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 | num2;
+                textBox5.Text = result.ToString();
+            }
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox3.Text) | String.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox3.Text = "16";
+                textBox4.Text = "2";
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 ^ num2;
+                textBox5.Text = result.ToString();
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 ^ num2;
+                textBox5.Text = result.ToString();
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (String.IsNullOrEmpty(textBox3.Text) | String.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox3.Text = "16";
+                textBox4.Text = "2";
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 << num2;
+                int result2 = result << 1;
+                textBox5.Text = result2.ToString();
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 << num2;
+                int result2 = result << 1;
+                textBox5.Text = result2.ToString();
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(textBox3.Text) | String.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox3.Text = "16";
+                textBox4.Text = "2";
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 << num2;
+                int result2 = result << num2;
+                textBox5.Text = result2.ToString();
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(textBox3.Text);
+                int num2 = Convert.ToInt32(textBox4.Text);
+                int result = num1 << num2;
+                int result2 = result << num2;
+                textBox5.Text = result2.ToString();
+            }
+        }
+    }
+}
